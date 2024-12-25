d5d26ddc8990e7cf033769fb37c2ae1376d0d45b
andreysavart <andrejsavkin751@gmail.com>
2024-12-25 03:36:10 +0300

    Diplom_20

--- Diplom_20/Tortoise_ORM/app.py
+++
@@ -1,9 +0,0 @@
-from tortoise import Tortoise, run_async
-
-async def init():
-    await Tortoise.init(
-        db_url='postgres://user:password@localhost:5432/university_db',
-        modules={'models': ['models']}
-    )
-    await Tortoise.generate_schemas()
-
